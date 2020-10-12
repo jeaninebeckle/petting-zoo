753fff1a8a9ef75b3a072e5665f4cbcb51912234
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-10-12 09:56:00 -0500

    all animal classes created

--- models/animals.py
+++ models/animals.py
@@ -44,3 +44,93 @@ class Sheep():
 		self.walking = True
 		
 sheep = Sheep('Spongebob Squarepants', 'sheep', date.today())
+
+class Copperhead():
+	def __init__(self, name, species, date_added):
+		self.name = name
+		self.species = species
+		self.date_added = date_added
+		self.slithering = True
+		
+copperhead = Copperhead('Captain Crunch', 'copperhead', date.today())
+
+class Python():
+	def __init__(self, name, species, date_added):
+		self.name = name
+		self.species = species
+		self.date_added = date_added
+		self.slithering = True
+		
+python = Python('Peter Pettigrew', 'python', date.today())
+
+class Cottonmouth():
+	def __init__(self, name, species, date_added):
+		self.name = name
+		self.species = species
+		self.date_added = date_added
+		self.slithering = True
+		
+cottonmouth = Cottonmouth('Chewbacca', 'cottonmouth', date.today())
+
+class Kingsnake():
+	def __init__(self, name, species, date_added):
+		self.name = name
+		self.species = species
+		self.date_added = date_added
+		self.slithering = True
+		
+kingsnake = Kingsnake('Kirby', 'kingsnake', date.today())
+
+class Anaconda():
+	def __init__(self, name, species, date_added):
+		self.name = name
+		self.species = species
+		self.date_added = date_added
+		self.slithering = True
+		
+anaconda = Anaconda('Aragorn', 'anaconda', date.today())
+
+class Mallard():
+	def __init__(self, name, species, date_added):
+		self.name = name
+		self.species = species
+		self.date_added = date_added
+		self.swimming = True
+		
+mallard = Mallard('Minerva McGonagall', 'mallard', date.today())
+
+class Goldfish():
+	def __init__(self, name, species, date_added):
+		self.name = name
+		self.species = species
+		self.date_added = date_added
+		self.swimming = True
+		
+goldfish = Goldfish('Godzilla', 'goldfish', date.today())
+
+class Swan():
+	def __init__(self, name, species, date_added):
+		self.name = name
+		self.species = species
+		self.date_added = date_added
+		self.swimming = True
+		
+swan = Swan('Severus Snape', 'swan', date.today())
+
+class Catfish():
+	def __init__(self, name, species, date_added):
+		self.name = name
+		self.species = species
+		self.date_added = date_added
+		self.swimming = True
+		
+catfish = Catfish('Cheshire Cat', 'catfish', date.today())
+
+class Bass():
+	def __init__(self, name, species, date_added):
+		self.name = name
+		self.species = species
+		self.date_added = date_added
+		self.swimming = True
+		
+bass = Bass('Beetlejuice', 'bass', date.today())
