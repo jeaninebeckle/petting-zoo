b4f979453595840a0f5606055ec1e1bba2165654
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-10-12 09:58:45 -0500

    add classes to init

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,1 @@
-from .animals import Donkey, Goat, Pig, Llama, Sheep
+from .animals import Donkey, Goat, Pig, Llama, Sheep, Copperhead, Python, Cottonmouth, Kingsnake, Anaconda, Mallard, Goldfish, Swan, Catfish, Bass
