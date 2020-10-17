9c368aba7d0de530dc89ae207f9b0e73074bbc31
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-10-17 10:42:55 -0500

    add shifts to walking animals

--- models/animals/goat.py
+++ models/animals/goat.py
@@ -1,10 +1,13 @@
 from datetime import date
 
 class Goat():
-	def __init__(self, name, species):
+	def __init__(self, name, species, shift):
 		self.name = name
 		self.species = species
+		self.shift = shift
 		self.date_added = date.today()
 		self.walking = True
 
-goat = Goat('Gandalf the Grey', 'goat')
+goat = Goat('Gandalf the Grey', 'goat', 'midday')
+
+print(f'{goat.name} the {goat.species} is available to pet during the {goat.shift} shift.')
--- models/animals/pig.py
+++ models/animals/pig.py
@@ -1,10 +1,13 @@
 from datetime import date
 
 class Pig():
-	def __init__(self, name, species):
+	def __init__(self, name, species, shift):
 		self.name = name
 		self.species = species
+		self.shift = shift
 		self.date_added = date.today()
 		self.walking = True
 
-pig = Pig('Peter Pan', 'pig')
+pig = Pig('Peter Pan', 'pig', 'afternoon')
+
+print(f'{pig.name} the {pig.species} is available to pet during the {pig.shift} shift.')
