8de9e57230d7b3f64f701c9a11b808ee8eeadf48
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-10-27 18:34:40 -0500

    attractions have restrictions with duck typing

--- attractions/pettingzoo.py
+++ attractions/pettingzoo.py
@@ -1,8 +1,18 @@
 from attractions.attraction import Attraction
+from movements import Walking
+
 
 class PettingZoo(Attraction):
   def __init__(self, attraction_name, description):
     super().__init__(attraction_name, description)
+
+  def add_animal_pythonic(self, animal):
+    try:
+      if animal.walk_speed > -1:
+        self.animals.append(animal)
+        print(f'{animal.name} the {animal.species} now lives in {self.attraction_name}')
+    except AttributeError as ex:
+      print(f'{animal.name} doesn\'t like to be petted, so please do not put it in the {self.attraction_name} attraction.')
 
   @property
   def last_critter_added(self):
--- attractions/snakepit.py
+++ attractions/snakepit.py
@@ -1,9 +1,18 @@
 from attractions.attraction import Attraction
+from movements import slithering
 
 class SnakePit(Attraction):
   def __init__(self, attraction_name, description):
     super().__init__(attraction_name, description)
 
+  def add_animal_pythonic(self, animal):
+    try:
+      if animal.slither_speed > -1:
+        self.animals.append(animal)
+        print(f'{animal.name} the {animal.species} now lives in {self.attraction_name}')
+    except AttributeError as ex:
+      print(f'{animal.name} is not a reptile, so please do not put it in the {self.attraction_name} attraction.')
+  
   @property
   def last_critter_added(self):
     return f'{self.animals[-1].name} was the last critter added to {self.attraction_name}'
